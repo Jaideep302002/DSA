712bb66c5b30f649f606bd70685032a95417bce1
Jaideep Singh <105032931+Jaideep302002@users.noreply.github.com>
2024-12-30 23:03:54 +0530

    Add files via upload

---
+++ Intro.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+using namespace std;
+void printArray(int arr[], int size)
+{
+
+    cout << "printing the array" << endl;
+
+    for (int i = 0; i < size; i++)
+    {
+        cout << arr[i] << " ";
+    }
+
+    cout << "printing done" << endl;
+}
+
+int main()
+{
+
+    // declare
+    int number[15];
+
+    cout << endl
+         << "Everything is fine" << endl;
+
+        return 0;
+}
